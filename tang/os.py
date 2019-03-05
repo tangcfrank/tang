3d9880de35ace8c4002c747808bdd9dd6300be11
tangcfrank <tc_frank@163.com>
2019-03-05 15:02:53 +0800

    11

---
+++ tang/os.py
@@ -0,0 +1,32 @@
+import os,sys
+import random
+#os.getcwd()
+#print(os.getcwd())
+#result = 0
+'''for i in range(10):
+    print(random.randint(1,10))
+    result +=1
+print(result)'''
+
+
+#os.mkdir('A')
+#os.mkdir('D:\\tangtang\\tang\\A\\B')
+#os.makedirs('D:\\tangtang\\tang\\C\\C')
+#os.remove('D:\\tangtang\\tang\\A\\B\\123')
+#os.remove('D:\\tangtang\\tang\\A\\B\\123.txt')
+#os.rmdir('D:\\tangtang\\tang\\A')
+#os.removedirs('D:\\tangtang\\tang\\D')
+#print("目录为：%s" %os.listdir(os.getcwd()))
+#print(os.getcwd())
+#os.removedirs('D:\\tangtang\\tang\\D\\tangtang\\tang\\C\\C')
+#os.makedirs('D:\\tangtang\\tang\\A\\B\\D')
+#os.removedirs('D:\\tangtang\\tang\\A\\B\\D')
+#os.system('cmd')
+'''print(os.curdir)
+print(os.listdir('.'))
+
+print(os.listdir(os.curdir))
+print(os.sep)
+print(os.name)'''
+
+#os.path
